53629fd83e7143fe24fcbaf45880c844fc7d72e8
realcold0 <realcold0@gmail.com>
2024-09-26 17:19:09 +0900

    fix : import 오류 수정

--- src/main/java/com/idle/kb_i_dle_backend/community/service/CommunityServiceImpl.java
+++ src/main/java/com/idle/kb_i_dle_backend/community/service/CommunityServiceImpl.java
@@ -1,7 +1,7 @@
 package com.idle.kb_i_dle_backend.community.service;
 
 import com.idle.kb_i_dle_backend.community.dto.BoardDTO;
-import com.idle.kb_i_dle_backend.community.repository.CommunityRepository;
+//import com.idle.kb_i_dle_backend.community.repository.CommunityRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
