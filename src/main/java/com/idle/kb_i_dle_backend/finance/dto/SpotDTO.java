be3eaf0399276064068d280274dccb20f07228e7
find11570 <find11570@naver.com>
2024-09-27 14:57:40 +0900

    feat : 현물 자산 CRUD, 현물자산 총액, 카테고리별 현물 자산

--- src/main/java/com/idle/kb_i_dle_backend/finance/dto/SpotDTO.java
+++ src/main/java/com/idle/kb_i_dle_backend/finance/dto/SpotDTO.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PriceSumDTO {
-    private String prodCategory;
-    private Long amount;
+public class SpotDTO {
+    private Integer index;
+    private String category;
+    private String name;
+    private Long price;
+    private String addDate;
 }
