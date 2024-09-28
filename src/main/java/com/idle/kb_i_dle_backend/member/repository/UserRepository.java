9f3fdd19e6fbbd6b22a887b1b71cd4e12702c05c
kogunwoo <wnwn8945@naver.com>
2024-09-28 11:27:55 +0900

    feat : 유저정보 조회,회원 수정 탈퇴

--- src/main/java/com/idle/kb_i_dle_backend/member/repository/UserRepository.java
+++ src/main/java/com/idle/kb_i_dle_backend/member/repository/UserRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByNickname(String nickname);
+    //추가
     Optional<User> findByUid(Integer uid);
 }
 
