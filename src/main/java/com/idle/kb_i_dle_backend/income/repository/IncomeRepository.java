0365e4614ee59e91c1cdac0bd1f5bd6c39c8fa2f
realcold0 <realcold0@gmail.com>
2024-10-01 00:20:05 +0900

    fix : IncomeRepository 중복 및 autowired수정

--- src/main/java/com/idle/kb_i_dle_backend/income/repository/IncomeRepository.java
+++ src/main/java/com/idle/kb_i_dle_backend/income/repository/IncomeRepository.java
@@ -1,8 +1,14 @@
 package com.idle.kb_i_dle_backend.income.repository;
 
 import com.idle.kb_i_dle_backend.income.entity.Income;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IncomeRepository extends CrudRepository<Income, Integer> {
     Income findByIndex(int id);
+
+    List<Income> findByUidAndDateBetween(int uid, Date startDate, Date endDate);
 }
