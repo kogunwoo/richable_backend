0734e0b2e4d5f36bc91d17fd9f711e4c25eda278
realcold0 <realcold0@gmail.com>
2024-10-02 01:19:12 +0900

    feat : 목표 저장 컨트롤러 생성

--- src/main/java/com/idle/kb_i_dle_backend/domain/goal/controller/GoalController.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/goal/controller/GoalController.java
@@ -1,9 +1,30 @@
 package com.idle.kb_i_dle_backend.domain.goal.controller;
 
+import com.idle.kb_i_dle_backend.common.dto.ResponseDTO;
+import com.idle.kb_i_dle_backend.domain.goal.dto.AddGoalDTO;
+import com.idle.kb_i_dle_backend.domain.goal.dto.GoalDTO;
+import com.idle.kb_i_dle_backend.domain.goal.service.GoalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/goal")
+@RequiredArgsConstructor
 public class GoalController {
+
+    private final GoalService goalService;
+
+    @PostMapping("/set")
+    public ResponseEntity<ResponseDTO> setGoal(@RequestBody AddGoalDTO goal){
+        try{
+            GoalDTO goalDTO = goalService.saveGoal(1,goal);
+            return ResponseEntity.ok(new ResponseDTO(true, goalDTO));
+        }catch (Exception e){
+            return ResponseEntity.ok(new ResponseDTO(false, e.getMessage()));
+        }
+    }
 }
