cc588a37d57eb14e24a755c03b93a57ae26a924a
realcold0 <realcold0@gmail.com>
2024-10-02 01:17:51 +0900

    feat : GoalRepository 목표 저장 유저의 해당카테고리 목표 갯수 조회 추가

--- src/main/java/com/idle/kb_i_dle_backend/domain/goal/repository/GoalRepository.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/goal/repository/GoalRepository.java
@@ -1,10 +1,13 @@
 package com.idle.kb_i_dle_backend.domain.goal.repository;
 
 import com.idle.kb_i_dle_backend.domain.goal.entity.Goal;
+import com.idle.kb_i_dle_backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Integer> {
     Goal findByIndex(int index);
+    Goal save(Goal goal);
+    Integer countByUidAndCategory(Member uid, String category);
 }
