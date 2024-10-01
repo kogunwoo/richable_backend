bae73a5fc7742b65405a624aa767dcb9ab3a2445
realcold0 <realcold0@gmail.com>
2024-10-02 01:18:15 +0900

    feat : 목표  interface 저장 메서드 명시

--- src/main/java/com/idle/kb_i_dle_backend/domain/goal/service/GoalService.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/goal/service/GoalService.java
@@ -1,4 +1,18 @@
 package com.idle.kb_i_dle_backend.domain.goal.service;
 
+import com.idle.kb_i_dle_backend.domain.goal.dto.AddGoalDTO;
+import com.idle.kb_i_dle_backend.domain.goal.dto.GoalDTO;
+import com.idle.kb_i_dle_backend.domain.member.entity.Member;
+
 public interface GoalService {
+
+
+    //목표 저장 메서드
+    GoalDTO saveGoal(int uid ,AddGoalDTO addGoalDTO) throws Exception;
+
+    //소비 목표 저장 메서드
+    GoalDTO saveOutcomeGoal(Member member, AddGoalDTO addGoalDTO);
+    
+    //자산 목표 저장 메서드
+    GoalDTO saveAssetGoal(Member member, AddGoalDTO addGoalDTO) throws Exception;
 }
