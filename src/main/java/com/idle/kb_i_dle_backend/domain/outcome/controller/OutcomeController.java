523044ca1a1021a8d3a654e7ebe0d28f17e2fa33
realcold0 <realcold0@gmail.com>
2024-10-01 15:10:23 +0900

    refactor : response DTO 로 수정

--- src/main/java/com/idle/kb_i_dle_backend/domain/outcome/controller/OutcomeController.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/outcome/controller/OutcomeController.java
@@ -48,17 +48,6 @@ public class OutcomeController {
     @GetMapping("/review/sum/{cntYear}/{cntMonth}")
     public ResponseEntity<ResponseDTO> saveOutcomeInMonth(@PathVariable int cntYear, @PathVariable int cntMonth){
         Calendar calendar = Calendar.getInstance();
-
-        // 시작일 설정 (해당 연도와 월의 첫 번째 날)
-        calendar.set(Calendar.YEAR, cntYear);
-        calendar.set(Calendar.MONTH, cntMonth - 1); // Calendar.MONTH는 0-based이므로 1을 빼줍니다.
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        Date startDate = calendar.getTime();
-
-        // 마지막 날 설정 (해당 연도와 월의 마지막 날)
-        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
-        Date endDate = calendar.getTime();
-
         PossibleSaveOutcomeInMonthDTO possibleSaveOutcomeInMonthDTO = outcomeService.findPossibleSaveOutcome(1, cntYear, cntMonth);
         ResponseDTO responseDTO = new ResponseDTO(true, possibleSaveOutcomeInMonthDTO);
         return ResponseEntity.ok(responseDTO);
@@ -72,7 +61,7 @@ public class OutcomeController {
      * @return
      */
     @GetMapping("/compare/{cntYear}/{cntMonth}/{category}")
-    public ResponseEntity<CompareAverageCategoryOutcomeDTO> compareWithAverage(@PathVariable int cntYear,  @PathVariable int cntMonth,  @PathVariable String category) {
+    public ResponseEntity<ResponseDTO> compareWithAverage(@PathVariable int cntYear,  @PathVariable int cntMonth,  @PathVariable String category) {
         Calendar calendar = Calendar.getInstance();
 
         // 시작일 설정 (해당 연도와 월의 첫 번째 날)
@@ -86,7 +75,7 @@ public class OutcomeController {
         Date endDate = calendar.getTime();
 
         CompareAverageCategoryOutcomeDTO compareAverageCategoryOutcomeDTO = outcomeService.compareWithAverage(1,  cntYear, cntMonth , category);
-        return ResponseEntity.ok(compareAverageCategoryOutcomeDTO);
+        return ResponseEntity.ok(new ResponseDTO(true, compareAverageCategoryOutcomeDTO));
     }
 
 
@@ -97,10 +86,10 @@ public class OutcomeController {
     }
 
     @GetMapping("/category/dailysum/{cntYear}/{cntMonth}")
-    public ResponseEntity<MonthOutcomeDTO> monthConsume(@PathVariable int cntYear, @PathVariable int cntMonth){
+    public ResponseEntity<ResponseDTO> monthConsume(@PathVariable int cntYear, @PathVariable int cntMonth){
         System.out.println("month!!!!!!!!!!!");
         MonthOutcomeDTO monthOutcomeDTO = outcomeService.findMonthOutcome(cntYear, cntMonth);
-        return ResponseEntity.ok(monthOutcomeDTO);
+        return ResponseEntity.ok(new ResponseDTO(true, monthOutcomeDTO));
     }
 
     // 소비 CRUD
