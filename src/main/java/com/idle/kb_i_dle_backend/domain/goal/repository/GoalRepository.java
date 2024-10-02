cdb15438a3270cb444bade429251300e7ac21257
realcold0 <realcold0@gmail.com>
2024-10-02 11:04:37 +0900

    feat : 유저가 가진 소비 목표 조회 repository 정의

--- src/main/java/com/idle/kb_i_dle_backend/domain/goal/repository/GoalRepository.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/goal/repository/GoalRepository.java
@@ -5,11 +5,14 @@ import com.idle.kb_i_dle_backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Integer> {
     Goal findByIndex(int index);
     Goal save(Goal goal);
     Integer countByUidAndCategoryAndIsAchive(Member uid, String category, boolean isAchive);
+    List<Goal> findByUidAndCategoryAndIsAchive(Member uid, String category, boolean isAchive);
     boolean existsByUidAndIndex(Member uid, int index);
     Integer deleteByUidAndIndex(Member uid, int index);
 }
