5efc0a9f8019631f31829d320dd5233231fb9b57
realcold0 <realcold0@gmail.com>
2024-10-10 19:19:48 +0900

    fix : jwt 토큰 만료 혹은 jwt 권한체크후 오류 처리

--- src/main/java/com/idle/kb_i_dle_backend/domain/member/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/member/handler/CustomAccessDeniedHandler.java
@@ -1,5 +1,9 @@
 package com.idle.kb_i_dle_backend.domain.member.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.idle.kb_i_dle_backend.global.codes.ErrorCode;
+import com.idle.kb_i_dle_backend.global.dto.ErrorResponseDTO;
+import com.idle.kb_i_dle_backend.global.response.ErrorResponse;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,8 +19,14 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
         // Return 403 Forbidden
-        response.setContentType("application/json;charset=UTF-8");
+        log.info("forbidden ");
+        final ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.FORBIDDEN_ERROR);
+        final ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(errorResponse);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        response.getWriter().write("{\"error\": \"Access Denied: " + accessDeniedException.getMessage() + "\"}");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String result = objectMapper.writeValueAsString(errorResponseDTO);
+        response.getWriter().write(result);
     }
 }
