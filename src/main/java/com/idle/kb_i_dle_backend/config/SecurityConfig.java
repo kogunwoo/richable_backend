80bc362a03d18a1c6ae6513e2c6eb2af4e5f869f
realcold0 <realcold0@gmail.com>
2024-10-12 03:04:22 +0900

    fix : cors오류 해결용

--- src/main/java/com/idle/kb_i_dle_backend/config/SecurityConfig.java
+++ src/main/java/com/idle/kb_i_dle_backend/config/SecurityConfig.java
@@ -20,8 +20,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -61,7 +61,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 쿠키나 인증 정보 허용
@@ -80,7 +80,7 @@ public class SecurityConfig {
         config.addAllowedMethod(HttpMethod.DELETE);
 
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        return source;
     }
 
     /**
