eb261619671625b1c7062d22731f309eac24fe53
realcold0 <realcold0@gmail.com>
2024-10-14 22:44:29 +0900

    fix : Badge 조회 Contorller 작성

--- src/main/java/com/idle/kb_i_dle_backend/domain/member/controller/UserBadgeController.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/member/controller/UserBadgeController.java
@@ -1,13 +1,13 @@
 package com.idle.kb_i_dle_backend.domain.member.controller;
 
 
-import com.idle.kb_i_dle_backend.domain.member.dto.UserBadgeDTO;
+import com.idle.kb_i_dle_backend.domain.member.dto.MemberBadgeDTO;
 import com.idle.kb_i_dle_backend.domain.member.service.UserBadgeService;
 import com.idle.kb_i_dle_backend.global.dto.SuccessResponseDTO;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,38 +23,29 @@ public class UserBadgeController {
 
     // 목표 달성 뱃지 조회
     @GetMapping("/badge/having/{nickname}")
-    public SuccessResponseDTO getAchievedBadges(@PathVariable String nickname) {
-        List<UserBadgeD> badges = userBadgeService.getUserBadges(nickname, true);
+    public ResponseEntity<SuccessResponseDTO> getAchievedBadges(@PathVariable String nickname) {
+        List<MemberBadgeDTO> badges = userBadgeService.getUserBadges(nickname, true);
 
         // response 감싸기
-        Map<String, Object> responseData = new HashMap<>();
-        responseData.put("data", badges);
+        SuccessResponseDTO successResponseDTO = new SuccessResponseDTO(true, badges);
 
-        return new SuccessResponseDTO("true", responseData);
+        return new ResponseEntity<>(successResponseDTO, HttpStatus.OK);
     }
 
     // 목표 미달성 뱃지 조회
     @GetMapping("/badge/nothaving/{nickname}")
-    public SuccessResponseDTO getNotAchievedBadges(@PathVariable String nickname) {
-        List<UserBadgeDTO> badges = userBadgeService.getUserBadges(nickname, false);
+    public ResponseEntity<SuccessResponseDTO> getNotAchievedBadges(@PathVariable String nickname) {
+        List<MemberBadgeDTO> badges = userBadgeService.getUserBadges(nickname, false);
 
-        // response 감싸기
-        Map<String, Object> responseData = new HashMap<>();
-        responseData.put("data", badges);
-
-        return new SuccessResponseDTO("true", responseData);
+        return new ResponseEntity<>(new SuccessResponseDTO(true, badges), HttpStatus.OK);
 
     }
 
     // 전체 뱃지 조회
     @GetMapping("/badge/all/{nickname}")
-    public SuccessResponseDTO getAllBadges(@PathVariable String nickname) {
-        List<UserBadgeDTO> badges = userBadgeService.getAllBadges(nickname);
+    public ResponseEntity<SuccessResponseDTO> getAllBadges(@PathVariable String nickname) {
+        List<MemberBadgeDTO> badges = userBadgeService.getAllBadges(nickname);
 
-        // response 감싸기
-        Map<String, Object> responseData = new HashMap<>();
-        responseData.put("data", badges);
-
-        return new SuccessResponseDTO("true", responseData);
+        return ResponseEntity.ok(new SuccessResponseDTO(true, badges));
     }
 }
