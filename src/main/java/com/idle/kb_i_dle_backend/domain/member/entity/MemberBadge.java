f7925608249f576fd74ed36c20982cb467b745b9
realcold0 <realcold0@gmail.com>
2024-10-14 22:44:19 +0900

    fix : MemberBadge Id 정의

--- src/main/java/com/idle/kb_i_dle_backend/domain/member/entity/MemberBadge.java
+++ src/main/java/com/idle/kb_i_dle_backend/domain/member/entity/MemberBadge.java
@@ -5,11 +5,15 @@ import com.idle.kb_i_dle_backend.global.entity.Badge;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.Getter;
 
+@Getter
 @Entity
+@IdClass(MemberBadgeId.class)
 @Table(name = "user_badge", catalog = "user_info")
 public class MemberBadge {
 
